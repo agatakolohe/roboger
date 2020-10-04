c6cfff61e23e4c9bd0a51710041ef5642a96c240
agatakolohe <agatakolohe@gmail.com>
2020-10-03 23:09:36 -0700

    turn array into string, if/else 1 & 2 to show  beep and boop in place on 1 & 2

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,24 @@ $(document).ready(function() {
 
     let numberRange = [];
     for (let i = 0; i <= inputNumbers; i += 1) {
-      numberRange.push(i);
+      numberRange.push(i.toString());
     };
-    console.log(inputNumbers);
+    //let beepRange =  numberRange.split(' ')
+    
+
+     numberRange.forEach(function(i) { 
+      //make new array to string from range
+       if (numberRange[i] === "1") {
+        numberRange[i] = "Beep!";
+       }
+       else if (numberRange[i] === "2") {
+         numberRange[i] = "Boop!";
+       }
+
+    });
+
+    console.log(numberRange);
     $("#result").text(numberRange);
-
-
 
   });
 });
