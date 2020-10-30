012943f46e357ead595f600bcb6ba50a0c15b3cc
agatakolohe <agatakolohe@gmail.com>
2020-10-30 09:30:48 -0700

    test map loop to return a range of numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,55 @@
-$(document).ready(function() {
-  $("form#roboger").submit(function(event) {
+//Business Logic
+// let numberRange = inputNumbers.map(number => {
+//   if (number.toString().split("").includes("3")) {
+//     numberRange.replace("3", "Wont'you be my neighbor?")
+//   }
+//   else if (number.toString().split("").includes("2")) {
+//     numberRange.replace("2", "Boop!")
+//   }
+//   else if (number.toString().split("").includes("")) {
+//     numberRange.replace("1", "Beep!")
+// });
+
+$(document).ready(function () {
+  $("form#roboger").submit(function (event) {
     event.preventDefault();
-    
     const inputNumbers = [];
     inputNumbers.push($("input#number").val());
-    let numberRange = [];
-    for (let i = 0; i <= inputNumbers; i += 1) {
-      numberRange.push(i.toString());
-    };
-    
-     numberRange.forEach(function(number) { 
-       if (numberRange[number].includes("3")) {
-        numberRange[number] ="Won't you be my neighbor?";
-       }
-       else if (numberRange[number].includes("2")) {
-         numberRange[number] = "Boop!";
-       }
-       else if (numberRange[number].includes("1")) {
-         numberRange[number] = "Beep!"
-       }
+
+    let numberRange = inputNumbers.map(function (number) {
+      if (number.toString().split("").includes("3")) {
+        inputNumbers.push(number, "Wont'you be my neighbor?");
+      } else if (number.toString().split("").includes("2")) {
+        inputNumbers.push(number, "Boop!");
+      } else if (number.toString().split("").includes("1")) {
+        inputNumbers.push(number, "Beep!");
+      }
+      return inputNumbers;
     });
+
     $("#result").text(numberRange);
   });
 });
+
+// for (let i = 0; i <= inputNumbers; i += 1) {
+//   numberRange.push(i.toString());
+// };
+// let numberRange = [];
+//  numberRange.forEach(function(number) {
+//    if (numberRange[number].includes("3")) {
+//     numberRange[number] ="Won't you be my neighbor?";
+//    }
+//    else if (numberRange[number].includes("2")) {
+//      numberRange[number] = "Boop!";
+//    }
+//    else if (numberRange[number].includes("1")) {
+//      numberRange[number] = "Beep!"
+//    }
+// });
+
+// if (number.toString().split("").includes("3")) {
+//   numberRange.replace(number, "Wont'you be my neighbor?");
+// } else if (number.toString().split("").includes("2")) {
+//   numberRange.replace(number, "Boop!");
+// } else if (number.toString().split("").includes("1")) {
+//   numberRange.replace(number, "Beep!");
